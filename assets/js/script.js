1425e494a2ff287f442f7cb9c793c1911e46fec2
Hideaki-Ryu <cenarahmant@gmail.com>
2023-07-13 18:26:23 +0700

    added cart counter function, modified color, remove unnecessary images

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,9 @@ const navOpenBtn = document.querySelector("[data-nav-open-btn]");
 const navbar = document.querySelector("[data-navbar]");
 const navCloseBtn = document.querySelector("[data-nav-close-btn]");
 const serviceItemText = document.querySelectorAll(".service-item-text");
+const productCategoryText = document.querySelectorAll(".filter-btn.active");
+const cardTitle = document.querySelectorAll(".card-title");
+const cardPrice = document.querySelectorAll(".card-price");
 
 const navElemArr = [overlay, navOpenBtn, navCloseBtn];
 
@@ -17,22 +20,56 @@ for (let i = 0; i < navElemArr.length; i++) {
   });
 }
 
-
-
 /**
  * add active class on header when scrolled 200px from top
  */
 
 window.addEventListener("scroll", function () {
-  window.scrollY >= 300 ? header.classList.add("active")
+  window.scrollY >= 300
+    ? header.classList.add("active")
     : header.classList.remove("active");
-})
+});
 
-const checkbox = document.getElementById("checkbox")
+/**
+ * add dark mode function
+ */
+
+const checkbox = document.getElementById("checkbox");
 checkbox.addEventListener("change", () => {
   document.body.classList.toggle("dark");
   navbar.classList.toggle("dark");
   serviceItemText.forEach((element) => {
     element.classList.toggle("dark");
   });
-})
+  productCategoryText.forEach((element) => {
+    element.classList.toggle("dark");
+  });
+  cardTitle.forEach((element) => {
+    element.classList.toggle("dark");
+  });
+  cardPrice.forEach((element) => {
+    element.classList.toggle("dark");
+  });
+});
+
+/**
+ * add cart counter function
+ */
+
+const cartBadge = document.querySelector(".cart-badge");
+
+const cartButtons = document.querySelectorAll(".cart-btn");
+const counterElement = document.getElementById("counterElement");
+
+let counter = 0;
+if (counter == 0) {
+  cartBadge.style.backgroundColor = "transparent";
+}
+
+cartButtons.forEach((cartButton) => {
+  cartButton.addEventListener("click", () => {
+    cartBadge.style.backgroundColor = "red";
+    counter++;
+    counterElement.textContent = counter.toString();
+  });
+});
